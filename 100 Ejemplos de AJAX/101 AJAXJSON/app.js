79c3003fd9e7d0e0e25c4723dbc1cc2c24bdf620
alan-serrano <alan.serrano.pch@gmail.com>
2019-09-13 16:18:00 -0400

    Insertando información de personas.json

--- 100 Ejemplos de AJAX/101 AJAXJSON/app.js
+++ 100 Ejemplos de AJAX/101 AJAXJSON/app.js
@@ -1,7 +1,7 @@
 const boton1 = document.getElementById('boton1');
 
 
-boton1.addEventListener('click', function clickListener() {
+boton1.addEventListener('click', function insertarInformacionPersona() {
     const xhr = new XMLHttpRequest;
 
     xhr.open('GET', 'empleado.json', true);
@@ -19,6 +19,36 @@ boton1.addEventListener('click', function clickListener() {
             `;
 
             document.getElementById('empleado').innerHTML = htmlTemplate;
+        }
+    }
+
+    xhr.send();
+})
+
+const boton2 = document.getElementById('boton2');
+
+boton2.addEventListener('click', function insertarInformacionPersonas() {
+    const xhr = new XMLHttpRequest;
+
+    xhr.open('GET', 'empleados.json', true);
+
+    xhr.onload = function onloadFunction() {
+        if(this.status === 200){
+            const personal = JSON.parse(this.responseText);
+            
+            let htmlTemplate = '';
+            personal.forEach(function forEach(persona) {
+                htmlTemplate += `
+                    <ul>
+                        <li>ID: ${persona.id}</li>
+                        <li>Nombre: ${persona.nombre}</li>
+                        <li>Empresa: ${persona.empresa}</li>
+                        <li>Trabajo: ${persona.trabajo}</li>
+                    </ul>
+                `;
+            });
+
+            document.getElementById('empleados').innerHTML = htmlTemplate;
         }
     }
 
